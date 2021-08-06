6eb6db9378cd59768d3aee5f5d3685d06af6005c
Dmitriy <q0pyo@yadnex.ru>
2021-08-06 14:27:20 +0300

    v.0.2

--- selenium_bs4/parser_combot.py
+++ selenium_bs4/parser_combot.py
@@ -1,9 +1,14 @@
 import typing
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 from bs4 import BeautifulSoup
+import os
+
+
+def main() -> None:
+    print("pls wait...")
+    # get_result_from_genirate()
+    get_all_parsing_pages()
 
 
 def web_driver_selenium(url):
@@ -22,9 +27,54 @@ def web_driver_selenium(url):
 
     except Exception as ex:
         print(ex)
+
     finally:
         driver.close()
         driver.quit()
+
+
+def get_all_pages():
+    options = webdriver.FirefoxOptions()
+    options.set_preference("general.useragent.override",
+                           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
+    try:
+        driver = webdriver.Chrome(
+            executable_path="/home/dima/PycharmProjects/marvel/selenium_bs4/geckodriver",
+            options=options
+        )
+        count = 0
+        for number_page in range(1,1831):
+            print(f".....{number_page}")
+            count += 1
+            url = f'https://combot.org/telegram/top/groups?lng=ru&page={number_page}&q=Знакомства'
+            driver.get(url=url)
+            time.sleep(1)
+            data = driver.page_source
+            with open(file=f"data/index_{count}.html", mode="w") as file:
+                file.write(data)
+
+    except Exception as ex:
+        print(ex)
+
+
+def get_all_parsing_pages():
+    folder = os.getcwd() + '/data'
+    list_sorted = []
+    for html in os.listdir(folder):
+        list_sorted.append(html)
+    filename = list_sorted
+    for file in filename:
+        if file.endswith(".html"):
+            html_path = os.path.join(folder, file)
+            with open(file=html_path, mode="r") as file:
+                src = file.read()
+            soup = BeautifulSoup(src, "lxml")
+            card_username = soup.find_all("span", class_="card-username")
+            for result in card_username:
+                item = str(result.text)
+                # yield item
+                with open(file="result.txt", mode="a") as file:
+                    file.writelines(item + "\n")
 
 
 def get_data():
@@ -34,15 +84,6 @@ def get_data():
     for result in card_username:
         item = result.text
         print(item)
-
-
-# def main() -> None:
-    # result = get_data()
-    # print(result)
-    # url = 'https://combot.org/telegram/top/groups?lng=ru&page=1&q=%D0%97%D0%BD%D0%B0%D0%BA%D0%BE%D0%BC%D1%81%D1%82%D0%B2%D0%B0'
-    # data = web_driver_selenium(url)
-    # write_to_html(data)
-
 
 
 def write_to_html(data):
